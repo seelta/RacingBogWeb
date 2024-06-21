2c29b50e90839bf41e78e799e1126b73108ff30a
Jose Miguel Acosta Imbachi <jose.acosta@unidadvictimas.gov.co>
2024-06-21 11:40:46 -0500

    primera version RacingBog Web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { DashboardRoutingModule } from './modules/dashboard/dashboard-routing.module';
 
-const routes: Routes = [];
+
+ const routes: Routes = [
+{ path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(
+        routes,
+        {enableTracing:false, useHash:true}        
+    ),
+    DashboardRoutingModule
+],
+    exports: [RouterModule],
+    
 })
 export class AppRoutingModule { }
